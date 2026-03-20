9ec74c1c318e0c871b4816a670a528db1e4c4a78
cgpvicto-2436889 <filoupoupo@gmail.com>
2026-03-20 11:23:09 -0400

    Modification pour que le progresql fonctionne

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,19 @@
-// Version du module mysql qu'on peut utiliser avec async/await
-import mysql from "mysql2/promise";
+import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-const pool = mysql.createPool({
-    connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+const { Pool } = pg;
+
+const pool = new Pool({
+    max: process.env.PG_CONNECTION_LIMIT || 10,
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    port: process.env.PG_PORT || 5432,
+    ssl: process.env.NODE_ENV === "production"
+        ? { rejectUnauthorized: false }
+        : false
 });
 
 export default pool;
--- src/models/pokemons.model.js
+++ src/models/pokemons.model.js
@@ -1,54 +1,27 @@
-
 import pool from '../config/db.js';
 
 const getPokemonSelonId = async (id) => {
 
-    // On récupère toutes les pokemons pour une langue donnée
     const requete = `
         SELECT nom, type_primaire, type_secondaire, pv, attaque, defense
         FROM pokemon
-
-        WHERE id = ?
+        WHERE id = $1
     `;
 
     try {
-        // mysql2 retourne [resultats, infos]
-        const [resultats] = await pool.query(requete, [id]);
+        const resultats = await pool.query(requete, [id]);
 
-        // On retourne tout le tableau (pas juste un élément)
-        return resultats;
+        // PostgreSQL retourne les résultats dans rows
+        return resultats.rows;
 
     } catch (erreur) {
-        console.log(`Erreur, code: ${erreur.code} sqlState ${erreur.sqlState} :
-                    ${erreur.sqlMessage}`);
+        console.log(`Erreur PostgreSQL :
+                    ${erreur.message}`);
         throw erreur;
     }
 };
 
-const getLangueSalutation = async () => {
-
-    // On récupère toutes les salutations pour une langue donnée
-    const requete = `
-        SELECT langue
-        FROM salutations
-    `;
-
-    try {
-        // mysql2 retourne [resultats, infos]
-        const [resultats] = await pool.query(requete);
-
-        // On retourne tout le tableau (pas juste un élément)
-        return [resultats];
-
-    } catch (erreur) {
-        console.log(`Erreur, code: ${erreur.code} sqlState ${erreur.sqlState} :
-                    ${erreur.sqlMessage}`);
-
-        throw erreur;
-    }
-};
 
 export default{
     getPokemonSelonId,
-    getLangueSalutation
 };
