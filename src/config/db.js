2dc0b7acd4413fde0fbb996aa5158fa590eb2c09
cgpvicto-2436889 <filoupoupo@gmail.com>
2026-03-20 11:44:28 -0400

    b

--- src/config/db.js
+++ src/config/db.js
@@ -5,15 +5,8 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-    max: process.env.PG_CONNECTION_LIMIT || 10,
-    host: process.env.PG_HOST,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-    port: process.env.PG_PORT || 5432,
-    ssl: process.env.NODE_ENV === "production"
-        ? { rejectUnauthorized: false }
-        : false
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
 export default pool;
